7e0a921a45b8b7b8c9a867fd7ba7299ca2eafc83
aleksandr-ardanov <baraboo312@gmail.com>
2021-04-26 14:40:13 -0500

    tables

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,14 +1,40 @@
 exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('user_username', 200).notNullable()
-      users.string('user_password', 200).notNullable()
-      users.string('user_email', 320).notNullable()
+      users.increments()
+      users.string('username', 200).notNullable().unique()
+      users.string('password', 200).notNullable()
+      users.string('phone_number', 320).notNullable()
       users.timestamps(false, true)
+    })
+    .createTable('species', tbl =>{
+      tbl.increments('species_id')
+      tbl.string('species_name').unique().notNullable()
+      tbl.string('h2oFrequency').notNullable()
+    })
+    .createTable('plants', tbl => {
+      tbl.increments('plant_id')
+      tbl.string('nickname').unique().notNullable()
+      tbl.integer('species_id')
+        .unsigned()
+        .notNullable()
+        .references('species_id')
+        .inTable('species')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT')
+      tbl.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT')
     })
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+  await knex.schema
+  .dropTableIfExists('plants')
+  .dropTableIfExists('species')
+  .dropTableIfExists('users')
 }
